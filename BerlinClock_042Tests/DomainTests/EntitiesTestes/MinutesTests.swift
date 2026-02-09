06b37a6a645eb3d4401de4201c1e493e92876d23
2026-DEV2-042 <dev_042@berlinclock.com>
2026-02-09 21:08:52 +0100

    Task(GREEN): Add minute entity to validate values beyond  range

--- BerlinClock_042Tests/DomainTests/EntitiesTestes/MinutesTests.swift
+++ BerlinClock_042Tests/DomainTests/EntitiesTestes/MinutesTests.swift
@@ -13,4 +13,12 @@ struct MinutesTests {
         //Assert
         #expect(result != nil)
     }
+    
+    @Test(arguments: [65, 77, 88, 99])
+    func validateMinutes_when_value_moreThanFifityNine(minutes: Int) {
+        //Act
+        let result = Minutes(minutes)
+        //Assert
+        #expect(result == nil)
+    }
 }
