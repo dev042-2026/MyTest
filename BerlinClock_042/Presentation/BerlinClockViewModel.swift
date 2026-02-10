61da63347f78bd78e3f091a44d9988d7393dedef
2026-DEV2-042 <dev_042@berlinclock.com>
2026-02-10 02:24:44 +0100

    Task(GREEN): ViewModel returns five-hour lamp state using FiveHour clock converter

--- BerlinClock_042/Presentation/BerlinClockViewModel.swift
+++ BerlinClock_042/Presentation/BerlinClockViewModel.swift
@@ -4,9 +4,15 @@ import Foundation
 class BerlinClockViewModel {
     
     private let secondsConverter = SecondsClockConverter()
+    private let fiveHoursConverter = FiveHourClockConverter()
     
     func secondsLampState(secondFieldData: Int) -> SecondsLamp {
         guard let seconds = Seconds(secondFieldData) else { return .off }
         return secondsConverter.convertSecondsClockToLamps(seconds: seconds)
+    }
+    
+    func fiveHourLampState(hourFieldData: Int) -> [FiveHourLamp] {
+        guard let hours = Hour(hourFieldData) else { return [] }
+        return fiveHoursConverter.convertToFiveHourLamp(hours: hours)
     }
 }
--- BerlinClock_042Tests/PresentationTests/BerlinClockViewModelTests.swift
+++ BerlinClock_042Tests/PresentationTests/BerlinClockViewModelTests.swift
@@ -16,4 +16,15 @@ struct BerlinClockViewModelTests {
         //Assert
         #expect(result == .on)
     }
+    
+    
+    @Test
+    func viewModel_when_HoursIsZero_fiveHourLamps_allOff() {
+        //Arrange
+        let sut = BerlinClockViewModel()
+        //Act
+        let result = sut.fiveHourLampState(hourFieldData: 00)
+        //Assert
+        #expect(result == [.noColor, .noColor, .noColor, .noColor])
+    }
 }
